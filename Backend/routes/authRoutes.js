cb18c35346a9b3d061b50ef40dab2cf5ad921dc8
SparklingStar212 <deborahalabi49@gmail.com>
2026-09-17 16:53:03 +0100

    chore: add a blank line for improved code readability in authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const router = express.Router();
 const { OAuth2Client } = require('google-auth-library');
 
+
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
